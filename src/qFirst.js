f318ac8ba39c64b3ce978ff2f5a14f27e7789b04
CJ Harries <cj@wizardsoftheweb.pro>
2017-06-12 09:52:11 -0500

    Tweak tests to better illustrate issue

--- src/qFirst.js
+++ src/qFirst.js
@@ -10,18 +10,18 @@ Initial constructor: ${Promise.prototype.constructor.name}`);
     const bluebird = require("./loadBluebird");
 
     return Promise.all([
-        bluebird.run()
-            .then((result) => {
-                logger.log("    Bluebird resolved with", result);
-                if (result !== true) {
-                    throw new Error("Bluebird failed to resolve");
-                }
-            }),
         q.run()
             .then((result) => {
                 logger.log("    Q resolved with ", result);
                 if (result !== true) {
                     throw new Error("Q failed to resolve");
+                }
+            }),
+        bluebird.run()
+            .then((result) => {
+                logger.log("    Bluebird resolved with", result);
+                if (result !== true) {
+                    throw new Error("Bluebird failed to resolve");
                 }
             })
     ]).then(() => {
--- test/comparison.js
+++ test/comparison.js
@@ -1,11 +1,17 @@
 const assert = require("assert");
+const saved = Object.assign(global.Promise);
 
 describe("Changing global.Promise in a module", () => {
+    beforeEach(() => {
+        global.Promise = saved;
+    });
+
     it("Loading Bluebird first shouldn't matter", () => {
         return require("../src/bluebirdFirst")().then((result) => {
             assert(result);
-        })
+        });
     });
+
     it("Loading Q first shouldn't matter", () => {
         return require("../src/qFirst")().then((result) => {
             return assert(result);
